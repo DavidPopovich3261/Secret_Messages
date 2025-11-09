0eab2ca8b7bc969cf7fb62b35e538acd4b53e076
DavidPopovich3261 <8534397@gmail.com>
2025-11-09 17:28:41 +0200

    ugiuyg

--- Mixed_stories.py
+++ Mixed_stories.py
@@ -5,6 +5,21 @@
 
 def fi(file):
     with open(file,"r") as f:
-        a=[]
-        for i in range(0,len(file),2):
-            a.append(f.readline())
+        a=''
+        b=''
+        for i in range(1,len(file),2):
+            a+=(f.readline())
+            b+=(f.readline())
+        a1=open('story_A.txt',"w")
+        a1.write(str(a))
+        b1=open("story_B.txt","w")
+        b1.write(str(b))
+        a1.close()
+        b1.close()
+        a2=open('story_A.txt')
+        print(a2.read())
+        b2=open("story_B.txt")
+        print(b2.read())
+
+
+fi('mixed_stories.txt')
